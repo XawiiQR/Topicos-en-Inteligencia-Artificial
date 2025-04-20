394cf96ac0aa6f10ff837364fb0223e3b80be37d
XawiiQR <qjavier617@gmail.com>
2025-04-19 20:07:24 -0500

    quick update

--- Laboratorio/Lab01/E3.cu
+++ Laboratorio/Lab01/E3.cu
@@ -6,17 +6,17 @@
 
 using namespace std;
 
-const int N = 1000;  // Puedes probar con más, como 1000000
+const int N = 1000;  
 vector<float> A(N), B(N), C(N);
 
-// Kernel CUDA
+
 __global__ void vectorAddGPU(const float* A, const float* B, float* C, int N) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < N)
         C[i] = A[i] + B[i];
 }
 
-// Inicializa A y B con valores aleatorios
+
 void CrearVectores(vector<float>& A, vector<float>& B) {
     srand(time(NULL));
     for (int i = 0; i < N; i++) {
@@ -25,42 +25,36 @@ void CrearVectores(vector<float>& A, vector<float>& B) {
     }
 }
 
-// Ejecuta la suma en GPU y mide tiempo
+
 float TiempoGPU(vector<float>& A, vector<float>& B, vector<float>& C) {
     float *d_A, *d_B, *d_C;
     size_t size = N * sizeof(float);
 
-    // Reservar memoria en la GPU
     cudaMalloc(&d_A, size);
     cudaMalloc(&d_B, size);
     cudaMalloc(&d_C, size);
 
-    // Copiar datos desde host a GPU
     cudaMemcpy(d_A, A.data(), size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, B.data(), size, cudaMemcpyHostToDevice);
 
-    // Configurar kernel
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Ejecutar kernel
     vectorAddGPU<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, N);
     cudaDeviceSynchronize();
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float, std::milli> duration = end - start;
 
-    // Copiar resultado de vuelta al host
     cudaMemcpy(C.data(), d_C, size, cudaMemcpyDeviceToHost);
 
-    // Liberar memoria en GPU
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
 
-    return duration.count(); // tiempo en milisegundos
+    return duration.count(); 
 }
 
 int main() {
